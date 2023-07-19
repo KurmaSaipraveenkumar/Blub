0b7d3e1f3e2eb46204c124ca58d1a45a189b01fd
KurmaSaipraveenkumar <121428946+KurmaSaipraveenkumar@users.noreply.github.com>
2023-07-19 21:46:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function off(){
+    document.getElementById("blub").src="https://www.pngwing.com/en/free-png-zrwfp";
+
+}
+function on(){
+    document.getElementById("blub").src="https://w7.pngwing.com/pngs/467/767/png-transparent-incandescent-light-bulb-light-bulb-free-electronics-text-hand-thumbnail.png"
+}
